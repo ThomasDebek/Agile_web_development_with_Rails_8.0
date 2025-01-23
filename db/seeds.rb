f3952907d74e94308b4bd1ab35a5d20409abedc8
ThomasDebek <tom.debek@gmail.com>
2025-01-23 23:33:23 +0100

    gem: add faker, created seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# db/seeds.rb
+
+require 'faker'
+
+# Usuń istniejące dane, jeśli chcesz zacząć od zera
+Product.destroy_all
+
+# Generuj 20 przykładowych produktów
+20.times do
+  Product.create!(
+    title: Faker::Commerce.product_name,
+    description: Faker::Lorem.sentence(word_count: 15),
+    price: Faker::Commerce.price(range: 10.0..1000.0, as_string: false)
+  )
+end
+
+puts "Seed data for products has been successfully created!"
